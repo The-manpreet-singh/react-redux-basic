cda7446792a4ddb89f85a558749030fe691382f4
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-11-18 14:54:57 +0530

    subscription method

--- redux-basics.js
+++ redux-basics.js
@@ -26,9 +26,15 @@ const rootReducer = (state= initialState, action) => {
 const store= createStore(rootReducer);
 console.log(store.getState()); // to verify the store
 
+
+//Subscription
+
+store.subscribe(()=>{
+    console.log('[Subscription]', store.getState());
+});
+
 //Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type:'ADD_COUNTER', value:10});
 console.log(store.getState());
 
-//Subscription
