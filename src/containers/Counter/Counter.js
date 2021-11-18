c5b5c1bf23aa0bcfa0685e0cee209b844ba9c1f2
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-11-18 17:10:19 +0530

    pass data with action

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,54 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import { connect } from 'react-redux';
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
+import { connect } from "react-redux";
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+  state = {
+    counter: 0,
+  };
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
+  counterChangedHandler = (action, value) => {
+    switch (action) {
+      case "inc":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + value };
+        });
+        break;
+      case "sub":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - value };
+        });
+        break;
+      default:
+        break;
     }
+  };
 
-    render () {
-        return (
-            <div>
-                {/* <CounterOutput value={this.state.counter} /> we use ctr name here for get state of redux */}
-                <CounterOutput value={this.props.ctr} />  
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {/* <CounterOutput value={this.state.counter} /> we use ctr name here for get state of redux */}
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCounter}
+        />
+        <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+        <CounterControl
+          label="Subtract 5"
+          clicked={this.props.onSubstractCounter}
+        />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-       ctr: state.counter //this state is a global state of redux reducer such as initialStat
-    }
+const mapStateToProps = (state) => {
+  return {
+    ctr: state.counter, //this state is a global state of redux reducer such as initialStat
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch({type: "INCREMENT"})
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({ type: "ADD", val: 5 }), // we use payload like value, name etc here no one stop us
+    onSubstractCounter: () => dispatch({ type: "SUBSTRACT", val: 5 }),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
