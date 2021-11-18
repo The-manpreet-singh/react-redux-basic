ec3a744d857a40ccec7489a7f3cf5cea1ea7ffc2
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-11-18 14:43:51 +0530

    dsipatch action

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState= {
 
 //Reducer- it takes 2 arguments old state and action
 const rootReducer = (state= initialState, action) => {
+    if(action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter:state.counter + 1
+        };
+    }
+    if(action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter:state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -15,6 +27,8 @@ const store= createStore(rootReducer);
 console.log(store.getState()); // to verify the store
 
 //Dispatching Action
-
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER', value:10});
+console.log(store.getState());
 
 //Subscription
